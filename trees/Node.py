eb27df9a032a5d26e2409c793743d48230d2673f
Ashly Thomas <ashlythomas@economist.com>
2025-02-23 11:08:35 +0000

    feat: insert function

---
+++ trees/Node.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = Node(value)
+            else:
+                self.left.insert(value)
+        elif value > self.value:
+            if self.right is None:
+                self.right = Node(value)
+            else:
+                self.right.insert(value)
+                
+
